1e657a9d38ac7a931e7ae12e3dedeae6dcfa99f1
Paulo Poiati <paulogpoiati@gmail.com>
2013-10-29 23:34:20 -0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-outbox',
+    version='0.1',
+    packages=['django_outbox'],
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='An mail outbox for development.',
+    long_description=README,
+    url='http://blog.paulopoiati.com/',
+    author='Paulo Poiati',
+    author_email='paulo@poiati.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
